d061172265e9b55f1d42f899165007a46be22f48
BIG <121756037+AhojHajzle@users.noreply.github.com>
2024-04-10 03:45:20 +0200

    Add files via upload

--- project zmrd.c
+++ project zmrd.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-//not working correctly chill
+//working correctly chill
 //hleda jestli cislo je nebo neni delitelne (prvocislo)
 //TO DO poupravit komentz
 #define cisloAno 0
 int main(){
 
     //user input, interface(?)
+    //z nejakejho douvoda musime zadat hodnotu dvakrat??? proc??? nechapu treba???
     int cisloOrig;
     
 
     printf("Remainder finder\nWrite your number\n\n");
     scanf("%d\n", &cisloOrig);
 
-    printf("%d\n", cisloOrig);
+    printf("%d\n", cisloOrig); 
     //variables se kteryma kod pracuje pro jasnost (snad)
-    //int start = 1;
-    int finish = cisloOrig-1;
+    int finish = cisloOrig-1; //patri k tutomu**
     int step = 1;
 
     int cislo = cisloOrig;
@@ -26,20 +26,20 @@ int main(){
 
 
     //main code (bro fr programuje ve dvou jazycich najednou)
-    //pridat goto statement, vratit se z if do while,
-    printf("%d = %d cislo -1\n\n", finish, cislo);
-    while(cisloOrig > finisher && finisher <= finish && finishec == false){ //sem, loopec
-        cislo = cislo % finisher;       //finisher se nepricita/neopakuje, porad deli nulou :(
+    //pridat goto statement, vratit se z if do while, (nee dement)
+    printf("%d = %d cislo -1\n\n", finish, cislo); //jenom test jestli se da takhle pocitat odpoved ano da**
+    while(cisloOrig > finisher && finisher <= finish && finishec == false){ //2. sem, loopec (loopec, to by asi se melo vratit ne??)*
+        cislo = cislo % finisher;       //finisher se nepricita/neopakuje, porad deli nulou :( (chyba, finisher pricital ale cislo se neresetovalo tudiz najednou aha)
         printf("funguju\n");  
-        if(cislo != cisloAno){                                              //odsud 
+        if(cislo != cisloAno){                                              //1. odsud (nic se nemusi vracet)*
             printf("nedelitene (zbytek %d)\n", cislo);
         }
-        else if(cislo == cisloAno){                                         //pripadne odsud (lmao dement koncime loop)
-            printf("%d delitelne", cislo);
+        else if(cislo == cisloAno){                                         
+            printf("delitelne (zbytek %d), deleno cislem %d", cislo, finisher);
             finishec = true; //nalezena delitelnost, koncime loop
         }
         finisher = finisher + step;
-        cislo = cisloOrig;        //nove pridane, vyresilo vsechno, nyni funguje
+        cislo = cisloOrig; //nove pridane, vyresilo vsechno, nyni funguje
     }
 
     
